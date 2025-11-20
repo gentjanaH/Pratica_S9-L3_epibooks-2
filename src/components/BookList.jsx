da820f04992336df0a38a9252fe16a5fdd1751be
Gentjana <hanigentjana@gmail.com>
2025-11-20 13:19:56 +0100

    add_filter

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,23 +1,43 @@
 import { Container, Row, Form } from 'react-bootstrap';
 import OneBook from "./OneBook";
+import { Component } from 'react'
 
-const BookList = function (props) {
-    return (
-        <Container fluid className="bg-dark px-5">
-            <Form className="d-flex justify-content-center">
-                <Form.Group className="mb-3 w-50 " controlId="formBasicEmail">
-                    <Form.Label></Form.Label>
-                    <Form.Control type="Text" placeholder="Cerca" className="bg-dark text-light" />
+class BookList extends Component {
+    state = {
+        searchQuery: "",
+    }
+    render() {
 
-                </Form.Group>
-            </Form>
-            <Row className="g-3">
-                {props.books.map((book) => (
-                    <OneBook key={book.asin} book={book} />
-                ))}
-            </Row>
-        </Container>
-    )
+        return (
+            <Container fluid className="bg-dark px-5">
+                <Form className="d-flex justify-content-center">
+                    <Form.Group className="mb-3 w-50 " controlId="formBasicEmail">
+                        <Form.Label></Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Cerca"
+                            value={this.state.searchQuery}
+                            onChange={(e) => {
+                                this.setState({ searchQuery: e.target.value })
+                            }}
+                            className="bg-secondary text-light" />
+
+                    </Form.Group>
+                </Form>
+                <Row className="g-3">
+                    {this.props.books
+                        .filter((book) =>
+                            book.title.toLowerCase().includes(this.state.searchQuery.toLocaleLowerCase())
+                        )
+                        .map((book) => (
+                            <OneBook key={book.asin} book={book} />
+                        ))}
+                </Row>
+            </Container>
+        )
+    }
 }
+
+
 
 export default BookList;
